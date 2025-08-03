5eac9285d9f09d9b01415d64fb3afbe017225381
HaDock404 <delescluse.g@gmail.com>
2025-08-03 09:01:26 +0200

    github page

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import './styles/normalize.css'
 import GlobalStyle from './styles/createGlobalStyle.jsx'
@@ -11,12 +11,12 @@ import Home from "./pages/Homepage.jsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <GlobalStyle/>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
